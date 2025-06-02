7f7b00d72fcec253f565fd807b5c21c8cdf3a49e
Shubb3006 <107855908+Shubb3006@users.noreply.github.com>
2025-06-02 11:45:02 +0530

    final commit

--- app/page.js
+++ app/page.js
@@ -1,27 +1,28 @@
-"use client";
+
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.info}>
-        <p>Hello, It's Me</p>
+        <h2>Hello, It's Me</h2>
         <h1>Shubham Bansal</h1>
-        <p>I'm a Full Stack Developer</p>
-        <p>flana dhimkana</p>
-        {/* <button
-          className={styles.btn}
-          onClick={() => {
-            const link = document.createElement("a");
-            link.href = "/Shubham_Resume.pdf"; // path inside public folder
-            link.download = "Shubham_Resume.pdf";
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-          }}
-        >
-          Download CV
-        </button> */}
+        <h3>I'm a Full Stack Developer</h3>
+        <p>
+          I specialize in building scalable and user-friendly web applications.
+          From responsive frontends using React and Next.js to efficient
+          backends with Node.js and PostgreSQL, I bring ideas to life through
+          clean, modern code.
+        </p>
+        <div className={styles.heroButtons}>
+          <Link href="projects" className={`${styles.btn} ${styles.primary}`}>
+            View Projects
+          </Link>
+          <Link href="contact" className={`${styles.btn} ${styles.secondary}`}>
+            Contact Me
+          </Link>
+        </div>
         <a
           href="/Shubham_Resume.pdf"
           download="Shubham_Bansal_CV.pdf"
@@ -29,14 +30,9 @@ export default function Home() {
         >
           Download CV
         </a>
-        
       </div>
       <div>
-        <img
-          className={styles.image}
-          src="/DP.jpg"
-          alt="Shubham"
-        />
+        <img className={styles.image} src="/DP.jpg" alt="Shubham" />
       </div>
     </div>
   );
--- components/DeployedProjects.jsx
+++ components/DeployedProjects.jsx
@@ -1,28 +1,59 @@
+"use client";
+import { useState, useEffect } from "react";
 import styles from "./DeployedProjects.module.css";
 import Link from "next/link";
 export default function DeployedProjects() {
-  const projects = [
-    {
-      id: "1",
-      name: "todo-app",
-      link: "https://todo-app-seven-sigma-83.vercel.app/",
-    },
-    {
-      id: "2",
-      name: "weather-app",
-      link: "https://weather-app-zeta-red-40.vercel.app/",
-    },
-    {
-      id: "3",
-      name: "github-user-app",
-      link: "https://github-user-coral.vercel.app/",
-    },
-    {
-      id: "4",
-      name: "My-portfolio",
-      link: "https://my-portfolio-one-psi-42.vercel.app/",
-    },
-  ];
+  const [projects, setProjects] = useState([]);
+  // const projects = [
+  //   {
+  //     id: "1",
+  //     name: "todo-app",
+  //     link: "https://todo-app-seven-sigma-83.vercel.app/",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "weather-app",
+  //     link: "https://weather-app-zeta-red-40.vercel.app/",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "github-user-app",
+  //     link: "https://github-user-coral.vercel.app/",
+  //   },
+  //   {
+  //     id: "4",
+  //     name: "My-portfolio",
+  //     link: "https://my-portfolio-one-psi-42.vercel.app/",
+  //   },
+  // ];
+  useEffect(() => {
+    async function fetchProjects() {
+      const res = await fetch("https://api.vercel.com/v6/deployments", {
+        headers: {
+          Authorization: "Bearer HxNy8k1Bk3PsZXAfah56NnQA",
+        },
+      });
+      if (!res.ok) throw new Error("could not load Projects");
+      const data = await res.json();
+
+      const latestDeploymentsMap = new Map();
+
+      data.deployments.forEach((deployment) => {
+        const existing = latestDeploymentsMap.get(deployment.name);
+        if (
+          !existing ||
+          new Date(deployment.createdAt) > new Date(existing.createdAt)
+        ) {
+          latestDeploymentsMap.set(deployment.name, deployment);
+        }
+      });
+
+      setProjects(Array.from(latestDeploymentsMap.values()));
+    }
+
+    fetchProjects();
+  }, []);
+
   return (
     <main className={styles.container}>
       <h1 className={styles.heading}>My Deployed Projects</h1>
@@ -32,12 +63,12 @@ export default function DeployedProjects() {
 
       <ul className={styles["project-list"]}>
         {projects.map((project) => (
-          <li key={project.id} className={styles["project-card"]}>
+          <li key={project.uid} className={styles["project-card"]}>
             <h2 className={styles["project-title"]}>
               {project.name.toUpperCase()}
             </h2>
             <a
-              href={project.link}
+              href={`https://${project.url}`}
               target="_blank"
               rel="noopener noreferrer"
               className="project-link"
